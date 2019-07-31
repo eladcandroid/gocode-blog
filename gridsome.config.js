c1ee7d8d0a0e77150f572300a22c554abfd4732c
Elad Cohen <eladc.android@gmail.com>
2019-07-31 17:52:32 +0300

    Remove / from start_url

--- gridsome.config.js
+++ gridsome.config.js
@@ -36,7 +36,7 @@ module.exports = {
       use: '~/gridsome-plugin-pwa',
       options: {
         title: 'GoCode Blog',
-        startUrl: '/index.html',
+        startUrl: 'index.html',
         display: 'standalone',
         statusBarStyle: 'default',
         // manifestPath: 'manifest.json',
